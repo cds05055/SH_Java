84c1dad9d93d5874fcc890567a0392d362a72e5b
com <com@222.111.72.201>
2023-07-03 23:56:43 +0900

    [ADD] 23/07/03 Member 클래스로 객체배열 복습

--- src/com/kh/day06/oop/member/Member.java
+++ src/com/kh/day06/oop/member/Member.java
@@ -3,4 +3,42 @@ package com.kh.day06.oop.member;
 public class Member {
 	// 입력 받을거 3개 정도 작성
 	// 캡슐화의 원칙 적용해서 구성
+	private String name;
+	private int score1, score2;
+	
+	public Member() {}
+	
+	public Member(String name, int score1, int score2) {
+		this.name = name;
+		this.score1 = score1;
+		this.score2 = score2;
+	}
+	
+	public double getAvg() {
+		double avg = (double)(this.score1 + this.score2)/3;
+		return avg;
+	}
+	
+	//get
+	public String getName() {
+		return this.name;
+	}
+	public int getScore1() {
+		return this.score1;
+	}
+	public int getScore2() {
+		return this.score2;
+	}
+	
+	//set
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setScore1(int score1) {
+		this.score1 = score1;
+	}
+	public void setScore2(int score2) {
+		this.score2 = score2;
+	}
+	
 }
--- src/com/kh/day06/oop/member/MemberRun.java
+++ src/com/kh/day06/oop/member/MemberRun.java
@@ -1,49 +1,101 @@
 package com.kh.day06.oop.member;
 
+import java.util.Scanner;
+
 public class MemberRun {
 
 	public static void main(String[] args) {
+		Member [] memArr = new Member[2];
+		Scanner sc = new Scanner(System.in);
+		String name = "";
+		int score1 = 0;
+		int score2 = 0;
 		
-		Member [] memArrs = new Member[3];
-		finish :
-		while(true) {
-			int choice = printMenu();
-			switch(choice) {
-			case 1 : 
-				inputScore(memArrs);
-				break;
-			case 2 :
-				printScore(memArrs);
-				break;
-			case 3 : 
-				checkPass(memArrs);
-				break;
-			case 4 : 
+		for(;;) {
+			int input = printMenu();
+			if(input == 1) {
+				inputScore(memArr);
+				
+			} else if(input == 2) {
+				printScore(memArr);
+				
+			} else if(input == 3) {
+				checkPass(memArr);
+				
+			} else if(input == 4) {
 				System.out.println("프로그램이 종료되었습니다.");
-				break finish;
+				break;
 			}
 		}
+		
 
 	}
-
-	private static void checkPass(Member[] memArr) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void printScore(Member[] memArr) {
-		// TODO Auto-generated method stub
-		
+	private static int printMenu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("=== 학생 성적 확인 프로그램 ===");
+		System.out.println();
+		System.out.println("1. 학생 성적 입력");
+		System.out.println("2. 학생 성적 출력");
+		System.out.println("3. 재평가 여부 확인");
+		System.out.println("4. 프로그램 종료");
+		System.out.print("메뉴 입력 : ");
+		int input = sc.nextInt();
+		return input;
 	}
 
 	private static void inputScore(Member[] memArr) {
-		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < memArr.length; i++) {
+			System.out.printf("%d번째 학생 정보 입력\n", i+1);
+			System.out.print("학생 이름 입력 : ");
+			String name = sc.next();
+			System.out.print("1차점수 입력 : ");
+			int score1 = sc.nextInt();
+			System.out.print("2차점수 입력 : ");
+			int score2 = sc.nextInt();
+			memArr[i] = new Member();
+			memArr[i].setName(name);
+			memArr[i].setScore1(score1);
+			memArr[i].setScore2(score2);
+		}
 	}
-
-	private static int printMenu() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	private static void printScore(Member[] memArr) {
+		for(int i = 0; i < 2; i++) {
+			System.out.printf("%d번째 학생 정보 출력\n", i+1);
+			System.out.printf("%s 학생의 점수는\n1차 점수 : %d점, 2차 점수 : %d점입니다.\n", 
+					memArr[i].getName(), memArr[i].getScore1(), memArr[i].getScore2());
+			System.out.println();					
+		}
+	}
+	
+	private static void checkPass(Member[] memArr) {
+		// 평균 60이상 합격이며 1, 2차의 점수가 40점 이상이면 합격
+		// 평균 60 이하 이면 불합격, 대신 1, 2차중 60점을 넘은 과목은 합격
+		for(int i = 0; i < memArr.length; i++) {
+			int num1 = memArr[i].getScore1();
+			int num2 = memArr[i].getScore2();
+			double avg = memArr[i].getAvg();
+			System.out.printf("%s 학생은 ",memArr[i].getName());
+			if(avg >= 60) {
+				if(num1 < 40) {
+					System.out.println("1차 재시험 대상입니다.");
+				} else if(num2 < 40) {
+					System.out.println("2차 재시험 대상입니다.");
+				}
+				if(num1 >= 40 && num2 >= 40){
+					System.out.println("통과입니다.");
+				}
+			} else if(avg < 60) {
+				if(num1 < 60) {
+					System.out.println("1차 재시험 대상입니다.");
+				}
+				if(num2 < 60) {
+					System.out.println("2차 재시험 대상입니다.");					
+				}
+			}
+		}
 	}
 
 }
