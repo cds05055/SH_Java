9e4498fc200beb58f84ddbd0c0b59ce9aa7e1414
cds05055 <cvb05055@gmail.com>
2023-07-12 17:47:05 +0900

    [ADD] 23/07/10 입출력 스트림과 파일 입출력 학습
    1. 입출력 스트림의 개념
    	- 입력 스트림의 read(), 출력 스트림write() 예제 실습 및 학습
    
    2. 스트림의 종류
    	- 바이트 스트림 : 이미지, 동영상 등 크기가 큰 파일 관련 스트림
    	- 문자 스트림 : 단순 문자 관련 스트림
    
    3. 파일 입출력
    	- java.io 패키지의 File 클래스 예제 실습
    	- File 객체의 메소드는 파일이름, 경로, 존재여부
    
    [ADD] 23/07/11 Music 첫 MVC 패턴 적용
    
    1. Model, View, Controller로 클래스의 역할을 나눠서 프로그램을 개발하는 방법 실습
    
    [FEAT] 23/07/11 음악관리 프로그램 정렬 추가
    
    1. 정렬 알고리즘을 이용하여 정렬 기능 추가
    	- 버블정렬
    	- 삽입정렬
    	- 선택정렬
    
    2. 문자열끼리 비교시 compareTo() 메소드 사용
    	- 0이면 동일, 양수라면 왼쪽이 큼, 음수라면 왼쪽이 작음
    
    [ADD] 23/07/12 자바 GUI 기초(AWT, Swing)
    
    1. Swing 기초 추가(feat.AWT)
    	- JFrame
    	- Container
    	- Layout
    	- Component
    
    2. Layout의 종류
    	- FlowLayout
    	- BoardLayout
    	- GridLayout
    	- CardLayout
    
    3. Component의 종류
    	- JButton
    	- JLabel
    	- JTextField 등등
    
    4. 이벤트 리스너 적용 및 예제 실습
    	- ActionListener
    	- MouseListener
    	- Adapter 클래스 사용하여 이벤트 구현
    
    [ADD] 23/07/12 자바 GUI - 키 이벤트 핸들러 등록
    
    1. 키 이벤트 핸들러 실습 예제
    2. 예제 문제(Exercise) 풀이

---
+++ src/com/kh/day13/swing/event/Exam_KeyListener.java
@@ -0,0 +1,51 @@
+package com.kh.day13.swing.event;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Exam_KeyListener extends JFrame{
+	
+	public Exam_KeyListener() {
+		setTitle("KeyListener 예제");	// 제목 설정
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// 닫을때까지 닫은게 아니여
+		JLabel [] laArrs = new JLabel[3];
+		laArrs[0] = new JLabel("getKeyCode()");
+		laArrs[1] = new JLabel("getKeyChar()");
+		laArrs[2] = new JLabel("getKeyText()");	// 컴포넌트 Label 초기값 설정
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.addKeyListener(new KeyAdapter() {		// Adapter 클래스 사용
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keyChar = e.getKeyChar();	// 유니코드 키 값
+				int keyCode = e.getKeyCode();	// 가상키 값
+				laArrs[0].setText(Integer.toString(keyCode));
+				laArrs[1].setText(Character.toString(keyChar));	// F5, F10 등 매핑 안되어있는건 안나옴
+				laArrs[2].setText(e.getKeyText(keyCode));
+			}
+		});
+		for(JLabel la : laArrs) {
+			la.setOpaque(true); // 바탕색 보이게 하기 위해서 컴포넌트를 불투명하게 함.
+			la.setBackground(Color.YELLOW);	// 바탕색 노란색으로 설정
+			c.add(la);			// 컴포넌트 콘테이너에 추가
+		}
+		
+		setSize(300, 150);	// Frame 크기 설정
+		setVisible(true);	// Frame visible 설정
+		// 스윙 프레임이 만들어질 포커스를 주고자 하는 경우 setVisible 이후에
+		// 포커스를 주어야 함.
+		// 키 입력을 받을 수 있도록 포커스를 주는 것임.
+		c.setFocusable(true);	// component가 포커스를 받을 수 있도록 설정
+		c.requestFocus();		// component에 포커스 강제 지정
+	}
+
+	public static void main(String[] args) {
+		new Exam_KeyListener();
+	}
+}
